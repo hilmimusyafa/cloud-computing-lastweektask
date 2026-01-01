e48e7084cbe886a9ea05f914495d03224be8c83f
hilmimusyafa <hilmimusyafa17@gmail.com>
2026-01-01 19:50:22 +0700

    init

---
+++ extermal-loadbalancer/edge-functions/load-balancer.js
@@ -0,0 +1,39 @@
+export default async (request) => {
+  const servers = [
+    "http://35.170.72.42/",
+    "http://54.234.156.133/"
+  ];
+
+  async function isAlive(server) {
+    try {
+      const res = await fetch(`${server}/health`, {
+        method: "GET",
+        timeout: 2000
+      });
+      return res.ok;
+    } catch {
+      return false;
+    }
+  }
+
+  const aliveServers = [];
+
+  for (const server of servers) {
+    if (await isAlive(server)) {
+      aliveServers.push(server);
+    }
+  }
+
+  if (aliveServers.length === 0) {
+    return new Response("All servers are down", { status: 503 });
+  }
+
+  // Simple random load balancing
+  const target =
+    aliveServers[Math.floor(Math.random() * aliveServers.length)];
+
+  const url = new URL(request.url);
+  const proxyUrl = target + url.pathname + url.search;
+
+  return fetch(proxyUrl, request);
+};
